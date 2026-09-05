11c9496b13c7450472d6ebea71f121b4db9f7bc2
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-09-05 20:02:21 +0530

    implement multer middleware for local file uploads to temporary storage

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import { diskStorage } from "multer";
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, '/public/temp');
+    },
+
+    filename(req, file, cb) {
+        const ext = file.mimetype.split("/")[1];
+        cb(null, file.originalname + '-' + Date.now() + ext);
+    },
+});
+
+export const upload = multer({ storage });

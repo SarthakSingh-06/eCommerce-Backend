002dadf5c1ed3e7bece955601f5c82a4b2ed01b3
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-09-06 13:31:44 +0530

    add forgot password email generation utility with mailgen

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 import { API_Error } from "./api-error.js";
+import Mailgen from "mailgen";
 
 const transport = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
@@ -9,6 +10,14 @@ const transport = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
     },
+});
+
+const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+        name: 'eCommerce.Backend',
+        link: 'http://localhost:8000',
+    }
 });
 
 const sendEmail = async ({ from, to, subject, text, html=undefined }) => {
@@ -24,6 +33,30 @@ const sendEmail = async ({ from, to, subject, text, html=undefined }) => {
     }
 };
 
+const generateForgotPasswordMail = (name, redirectLink) => {
+    const email = {
+        body: {
+            name,
+            intro: 'Welcome to eCommerce.Backend! We\'re very happy to help you with your issues.',
+            action: {
+                instructions: 'To get started with eCommerce.Backend, please click here:',
+                button: {
+                    color: '#2c65f5ff',
+                    text: 'Confirm your account',
+                    link: redirectLink
+                }
+            },
+            outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+        }
+    };
+
+    const emailHTML = mailGenerator.generate(email);
+    const emailText = mailGenerator.generatePlaintext(email);
+
+    return { emailText, emailHTML, };
+};
+
 export {
     sendEmail,
+    generateForgotPasswordMail,
 };
